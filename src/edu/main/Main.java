f2528e22153cfdc074da7914b98d113825360a36
naimyachou <naimyachou@10.5.29.159>
2024-10-23 00:52:20 +0200

    Input added

--- src/edu/main/Main.java
+++ src/edu/main/Main.java
@@ -6,6 +6,10 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		JFrame window = new JFrame();
+
+		String name = JOptionPane.showInputDialog("Enter your name!", "Mark Zuckenberg");
+		
+		JOptionPane.showMessageDialog(window, String.format("Your name is %s!" , name), "Username", JOptionPane.WARNING_MESSAGE);
 		
 		window.setSize(600, 800);
 		window.setVisible(true);
