04543b969680eaac759373fce4be444fd307078d
naimyachou <naimyachou@192.168.0.150>
2024-10-24 23:45:35 +0200

    Image added and file structure ordered.

--- src/edu/main/Main.java
+++ src/edu/main/Main.java
@@ -5,23 +5,17 @@ import java.awt.*;
 public class Main {
 
 	public static void main(String[] args) {
-		
 		JFrame window = new JFrame();
 		
 		String currentPath = System.getProperty("user.dir"); // Get absolute direction of the project
 		Image appIcon = Toolkit.getDefaultToolkit().getImage(currentPath + "/assets/icon.jpg"); 
         ImageIcon imageIcon = new ImageIcon(currentPath + "/assets/image.jpg");
 
-		
 		String name = JOptionPane.showInputDialog("Enter your name!", "Mark Zuckenberg");		
 		int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age!"));
 		boolean male = (JOptionPane.showConfirmDialog(window, "Are you male?") == 0) ? true : false;
-		
-		//System.out.printf("Gender: %b", male);
-		
 		String isMale = (male) ? "Male" : "Female";
 		
-		//window.setName("Best Game Ever!!! ");
 		window.setSize(600, 800);
 	    window.setIconImage(appIcon);
 		window.setVisible(true);
@@ -30,8 +24,7 @@ public class Main {
 		JLabel imageLabel = new JLabel(imageIcon);
         window.add(imageLabel);
 		
-		
-		//JOptionPane.showMessageDialog(window, String.format("Your name is %s! \nAge: %d \nGender: %s" , name, age, isMale), name + "\'s Data", JOptionPane.WARNING_MESSAGE);
+		JOptionPane.showMessageDialog(window, String.format("Your name is %s! \nAge: %d \nGender: %s" , name, age, isMale), name + "\'s Data", JOptionPane.WARNING_MESSAGE);
 	}
 
 }
