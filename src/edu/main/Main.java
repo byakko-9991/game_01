f48615584775c56ad32528c26756a411c0d8f49e
naimyachou <naimyachou@10.55.0.121>
2024-10-24 20:37:46 +0200

    Main changed.

--- src/edu/main/Main.java
+++ src/edu/main/Main.java
@@ -9,7 +9,6 @@ public class Main {
 		String name = JOptionPane.showInputDialog("Enter your name!", "Mark Zuckenberg");
 		
 		int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age!"));
-		//System.out.print(JOptionPane.showConfirmDialog(window, "Test"));
 		boolean male = (JOptionPane.showConfirmDialog(window, "Are you male?") == 0) ? true : false;
 		System.out.printf("Gender: %b", male);
 		
