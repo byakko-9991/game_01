c94927632c17b4ac3905bd87a548924fc64b43dd
naimyachou <naimyachou@10.55.0.121>
2024-10-24 20:57:27 +0200

    Icon added to window.

--- src/edu/main/Main.java
+++ src/edu/main/Main.java
@@ -1,13 +1,17 @@
 package edu.main;
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
 		JFrame window = new JFrame();
-		String name = JOptionPane.showInputDialog("Enter your name!", "Mark Zuckenberg");
 		
+		String currentPath = System.getProperty("user.dir"); // Get absolute direction of the project
+		Image appIcon = Toolkit.getDefaultToolkit().getImage(currentPath + "/assets/icon.png");  
+		
+		String name = JOptionPane.showInputDialog("Enter your name!", "Mark Zuckenberg");		
 		int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age!"));
 		boolean male = (JOptionPane.showConfirmDialog(window, "Are you male?") == 0) ? true : false;
 		System.out.printf("Gender: %b", male);
@@ -15,6 +19,7 @@ public class Main {
 		String isMale = (male) ? "Male" : "Female";
 		
 		window.setSize(600, 800);
+	    window.setIconImage(appIcon);
 		window.setVisible(true);
 		
 		JOptionPane.showMessageDialog(window, String.format("Your name is %s! \nAge: %d \nGender: %s" , name, age, isMale), name + "\'s Data", JOptionPane.WARNING_MESSAGE);
