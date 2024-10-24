43259a5e83fa150b363ae72eb90e3cf16e46e640
naimyachou <naimyachou@10.55.0.121>
2024-10-24 20:36:36 +0200

    Main changed!

--- src/edu/main/Main.java
+++ src/edu/main/Main.java
@@ -8,10 +8,17 @@ public class Main {
 		JFrame window = new JFrame();
 		String name = JOptionPane.showInputDialog("Enter your name!", "Mark Zuckenberg");
 		
+		int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age!"));
+		//System.out.print(JOptionPane.showConfirmDialog(window, "Test"));
+		boolean male = (JOptionPane.showConfirmDialog(window, "Are you male?") == 0) ? true : false;
+		System.out.printf("Gender: %b", male);
+		
+		String isMale = (male) ? "Male" : "Female";
+		
 		window.setSize(600, 800);
 		window.setVisible(true);
 		
-		JOptionPane.showMessageDialog(window, String.format("Your name is %s!" , name), "Username", JOptionPane.WARNING_MESSAGE);
+		JOptionPane.showMessageDialog(window, String.format("Your name is %s! \nAge: %d \nGender: %s" , name, age, isMale), name + "\'s Data", JOptionPane.WARNING_MESSAGE);
 	}
 
 }
